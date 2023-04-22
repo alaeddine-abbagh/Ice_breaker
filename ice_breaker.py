d606b298f5469a9f8e9e06ba6e083f3109f249e4
Eden Marco <emarco177@gmail.com>
2023-04-22 17:59:15 +0300

    working e2e fullstack application

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
@@ -9,7 +11,7 @@ from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 
 
-def ice_break(name: str) -> PersonIntel:
+def ice_break(name: str) -> Tuple[PersonIntel, str]:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -38,9 +40,10 @@ def ice_break(name: str) -> PersonIntel:
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
     result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
-    return person_intel_parser.parse(result)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
     print("Hello LangChain!")
     result = ice_break(name="Harrison Chase")
+    print(result)
